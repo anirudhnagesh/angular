9a0d9e8b33f4f4f681734afae6a3d415a7b925e1
Anirudh Nagesh <anagesh@proteus.com>
2016-07-11 21:03:09 -0700

    Moving styles to css and updates to views and controllers

--- elementum/app.js
+++ elementum/app.js
@@ -16,12 +16,16 @@ angularApp.config(['$routeProvider',function ($routeProvider) {
 	});
 }])
 
+
+// Assumption : Can include service and controllers in separate files as a good practice. But included
+// all of them in one file for this example.
 angularApp.service("MovieService", [
         '$http',
         function ($http) {
 
         	var movieService = {}
 
+            // can store all of these inside constants file and use it from there.
             var base = 'http://api.themoviedb.org/3';
             var service = '/collection/528';
             var apiKey = 'cf852df10c90128fb734af573f4f1a0d';
@@ -29,9 +33,17 @@ angularApp.service("MovieService", [
             var url = base + service + '?api_key=' + apiKey + '&callback=' + callback;
             var imageBaseUrl =  "https://image.tmdb.org/t/p/w500/"
 
+            var movie_background_path = ""
+
             movieService.getMovies = function () {
                 return $http.jsonp(url)
             };
+
+            movieService.getMovieBasicInfo = function (movieId) {
+                service  = "/movie/" + movieId
+                url = base + service + '?api_key=' + apiKey + '&callback=' + callback;
+                return $http.jsonp(url)
+            }
 
             movieService.getMovieCredits = function (movieId) {
                 service = "/movie/" + movieId + "/credits"
@@ -48,8 +60,7 @@ angularApp.controller('MasterDetailCtrl', ['$scope', function ($scope) {
 
 }]);
 
-angularApp.controller('moviesController', ['$scope','$routeParams','$http', 'MovieService', function ($scope,$routeParams,$http, MovieService) {
-	console.log("inside movies");
+angularApp.controller('moviesController', ['$scope','$routeParams','$http', 'MovieService','$location', function ($scope,$routeParams,$http, MovieService,$location) {
 
 	$scope.movies = []
 	$scope.movieBackgroundImages = []
@@ -61,8 +72,12 @@ angularApp.controller('moviesController', ['$scope','$routeParams','$http', 'Mov
                $scope.movieBackgroundImages.push(movie.backdrop_path)
         	})
     	})
+    }
 
-    	
+    // There could be a better way of adding style, but not aware of it.
+    $scope.movieSelected = function(id) {
+        document.getElementById('image'+id).style = "border:5px solid #1a9dba;"
+        $location.path('details/' + id)
     }
 
 
@@ -75,20 +90,43 @@ angularApp.controller('moviesController', ['$scope','$routeParams','$http', 'Mov
 
 }]);
 
-
+// Assumption : have used underscore to manipulate, filter and pick values from array.
 angularApp.controller('movieDetailsController', ['$scope','$routeParams','MovieService', function ($scope,$routeParams,MovieService) {
-	console.log("inside details movies");
-    console.log($routeParams.id)
     $scope.message = "Inside details"
 
     $scope.crews = []
     $scope.cast = []
-    $scope.director = []
+    $scope.director = ""
+    $scope.writers = ""
 
-        MovieService.getMovieCredits($routeParams.id).then(function(data) {
-            
-            $scope.cast = data.data.cast
-        })
+    $scope.movie_title = ""
+    $scope.profile_pic = ""
+
+    MovieService.getMovieBasicInfo($routeParams.id).then(function(data) {
+        $scope.backdrop_path = data.data.backdrop_path
+        $scope.movie_about = data.data.overview
+        $scope.movie_title = data.data.original_title
+        $scope.profile_pic = data.data.backdrop_path
+    })
+
+    MovieService.getMovieCredits($routeParams.id).then(function(data) {
+        $scope.cast = data.data.cast
+
+        $scope.director = _.find(data.data.crew, function(person) {
+            return person.department == "Directing"
+        }).name
+
+        $scope.stars = _.pluck(data.data.cast,'name').join(',')
+
+        $scope.writers = _.pluck(_.filter(data.data.crew, function(person) {
+            return person.department == "Writing"
+        }), 'name').join(',')
+        
+    })
+
+    $scope.loadProfiePic = function(image) {
+        $scope.profile_pic = image
+    }
 }]);
 
 
